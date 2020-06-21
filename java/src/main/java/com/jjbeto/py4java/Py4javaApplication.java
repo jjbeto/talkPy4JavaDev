a41e6ef89af89faf9a46abeec8f2d2783fa1f229
jjbeto <jjbeto@gmail.com>
2020-06-21 18:07:58 +0200

    init java app

---
+++ java/src/main/java/com/jjbeto/py4java/Py4javaApplication.java
@@ -0,0 +1,13 @@
+package com.jjbeto.py4java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Py4javaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Py4javaApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/jjbeto/py4java/Py4javaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjbeto.py4java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Py4javaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
